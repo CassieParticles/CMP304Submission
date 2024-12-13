59e11c085113ccc9927d5c758fe0d61b3a2afc58
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2024-12-13 03:43:06 +0000

    Added Gun prefab and class

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,6 +7,7 @@ public class Gun : MonoBehaviour
     protected float fireRate;
     protected float bulletSpread;
 
+    //Return GameObject if new bullet is fired, return null otherwise
     public virtual GameObject ShootAt(Vector2 direction)
     {
         return null;
--- Assets/Scripts/MachineGun.cs
+++ Assets/Scripts/MachineGun.cs
@@ -6,6 +6,7 @@ public class MachineGun : Gun   //Gun inherits from monobehavior
 {
     public override GameObject ShootAt(Vector2 direction)
     {
+        Debug.Log("Machine gun");
         return null;
     }
 }
