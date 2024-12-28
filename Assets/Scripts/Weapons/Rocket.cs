3a360e6761e840b8ba0f5825ac7c30a471438637
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2024-12-28 13:52:37 +0000

    Added scripts for rocket launcher

--- Assets/Scripts/Weapons/Rocket.cs
+++ Assets/Scripts/Weapons/Rocket.cs
@@ -4,5 +4,75 @@ using UnityEngine;
 
 public class Rocket : Bullet
 {
+    [SerializeField] private float explosionRadius = 4;
+    [SerializeField] private float maxExplosionDamage = 120;
+    [SerializeField] private float explosionTimer = 0.5f;
 
+    private BoxCollider2D rocketCollider;
+    private CircleCollider2D explosionCollider;
+    private SpriteRenderer rocketSprite;
+    private SpriteRenderer explosionSprite;
+
+    private bool exploded = false;  //Set to true once rocket collides (and explodes)
+
+    public override float calcDamage(Actor actor)
+    {
+        float distance = (transform.position - actor.transform.position).magnitude;
+
+        return maxExplosionDamage * distance;
+    }
+
+    private void Explode()
+    {   
+        //Set the rocket to having exploded
+        exploded = true;
+
+        //Disable rocket components
+        rocketCollider.enabled = false;
+        rocketSprite.enabled = false;
+
+        //Enable explosion components
+        explosionCollider.enabled = true;
+        explosionSprite.enabled = true;
+
+        StartCoroutine(ClearExplosion());
+    }
+
+    private IEnumerator ClearExplosion()
+    {
+        yield return new WaitForSeconds(explosionTimer);
+
+        Destroy(gameObject);
+    }
+
+    private void Awake()
+    {   
+        //Get components
+        rocketCollider = GetComponent<BoxCollider2D>();
+        explosionCollider = GetComponent<CircleCollider2D>();
+        rocketSprite = GetComponent<SpriteRenderer>();
+        explosionSprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+
+        //Set up components
+        explosionSprite.gameObject.transform.localScale = new Vector3(explosionRadius * 2, explosionRadius * 2, 1);
+        explosionCollider.radius = explosionRadius;
+        explosionCollider.enabled = false;
+        explosionSprite.enabled = false;
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!exploded)
+        {
+            Explode();
+        }
+        else 
+        {
+            Actor actor = collision.gameObject.GetComponent<Actor>();
+            if(actor)
+            {
+                actor.TakeDamage(calcDamage(actor));
+            }
+        }
+    }
 }
--- Assets/Scripts/Weapons/RocketLauncher.cs
+++ Assets/Scripts/Weapons/RocketLauncher.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class RocketLauncher : Gun
 {
-    
+    [SerializeField] private float rocketSpeed = 5;
+    [SerializeField] private int rocketCount = 1;
+
+    protected override IEnumerator Shoot(Vector2 target)
+    {
+        //Shoot gun
+        GameObject newBullet = Instantiate(bulletShot);
+
+        Vector2 direction = (target - new Vector2(transform.position.x, transform.position.y)).normalized;
+
+        newBullet.GetComponent<Rigidbody2D>().velocity = direction * rocketSpeed;
+        newBullet.transform.position = transform.position + new Vector3(direction.x, direction.y, 0);
+        newBullet.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x));
+
+        canShoot = false;
+
+        bulletCount--;
+
+        yield return null;  //Only 1 rocket, so no need to add checks and timers
+    }
+
+    public override int getGunValue()
+    {
+        return 80;
+    }
+
+    private void Start()
+    {
+        canShoot = true;
+        bulletCount = rocketCount;
+    }
 }
